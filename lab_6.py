13aa51853c026eea45ae88ea95dc259287756da2
allyrecalde <139785194+allyrecalde@users.noreply.github.com>
2023-07-18 19:33:28 -0400

    Add files via upload
    let's hope this works

---
+++ lab_6.py
@@ -0,0 +1,47 @@
+def display_menu():
+    print('Menu\n')
+    print('-------------\n')
+    print('1. Encode\n')
+    print('2. Decode\n')
+    print('3. Quit\n')
+
+
+def encode(password):
+    encoded = ''
+    for char in password:
+        encoded += str(int(char) + 3) #make each character in password and integer, then add them together into a string
+    return encoded #return string
+
+def decode(password):
+    decoded = ''
+    for char in password:
+        decoded += str(int(char) - 3) #make each character an int() and subtract 3 to get original inputted password
+    return decoded # return concatenated string
+
+password = '' #start password as empty string
+option = 0 # starting off point
+
+while option != 3:
+
+    display_menu()
+    option = int(input('Please enter an option: \n'))
+
+
+    if option == 1:
+        password = input('Please enter your password to encode: \n')
+        if len(password) != 8 or not password.isdigit:
+            print('Invalid password. Must be 8 digits long consisting of only integers')
+        else:
+            password1 = encode(password)
+            print('Your password has been encoded and stored!\n')
+    elif option == 2:
+        password2 = encode(password)
+        print(f'The encoded password is {password2}, and the original password is {password}.')
+    elif option == 3:
+        break
+    else:
+        print('Invalid option')
+
+
+
+
